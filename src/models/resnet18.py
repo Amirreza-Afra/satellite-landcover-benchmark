39795382e2430bdc22e49562dbff943784e2d044
Amir reza Afra <adres.afra@gmail.com>
2026-07-22 22:41:18 +0330

    fix:

--- src/models/resnet18.py
+++ src/models/resnet18.py
@@ -1,5 +1,9 @@
 import torch.nn as nn
-from torchvision.models import resnet18, ResNet18_Weights
+
+from torchvision.models import (
+    resnet18,
+    ResNet18_Weights
+)
 
 from .registry import register_model
 
@@ -11,15 +15,19 @@ class ResNet18Model(nn.Module):
         super().__init__()
         self.cfg = cfg
         num_classes = cfg["model"]["num_classes"]
-        self.model = resnet18(
-            weights=ResNet18_Weights.DEFAULT
+        pretrained = cfg["model"]["pretrained"]
+        weights = (
+            ResNet18_Weights.DEFAULT
+            if pretrained
+            else None
         )
+        self.model = resnet18(weights=weights)
         in_features = self.model.fc.in_features
         self.model.fc = nn.Linear(
             in_features,
             num_classes
         )
-        print("Pretrained ResNet18 Loaded")
+        print("ResNet18 Loaded")
 
     def forward(self, x):
         return self.model(x)
