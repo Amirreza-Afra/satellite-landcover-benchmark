71beab574dc0e4e3ef723b5a837bd086c466299c
Amir reza Afra <adres.afra@gmail.com>
2026-07-22 21:48:25 +0330

    feat: implement dataset registry and builder

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
 from src.configs.loader import load_config
+from src.datasets.builder import build_dataset
+
+# مهم: این import برای ثبت شدن دیتاست در Registry لازم است
+import src.datasets.eurosat
 
 
 def main():
+
     cfg = load_config("src/configs/base.yaml")
 
-    print(cfg)
+    dataset = build_dataset(cfg)
+
+    print(dataset)
 
 
 if __name__ == "__main__":
