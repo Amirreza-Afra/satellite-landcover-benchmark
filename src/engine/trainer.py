96229f9411672debded2995a0c1a51b107180e0d
Amir reza Afra <adres.afra@gmail.com>
2026-07-22 22:35:56 +0330

    feat: add device management to trainer

--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -1,17 +1,23 @@
 from src.datasets.builder import build_dataset
 from src.models.builder import build_model
+from src.utils.device import get_device
 
 
 class Trainer:
 
     def __init__(self, cfg):
         self.cfg = cfg
+        # Device
+        self.device = get_device()
+        # Components
         self.dataset = build_dataset(cfg)
         self.model = build_model(cfg)
+        # Move model to device
+        self.model.to(self.device)
         print("Trainer Initialized")
-
 
     def fit(self):
         print("Training Started...")
+        print(f"Device: {self.device}")
         print(self.dataset)
         print(self.model)
--- src/models/resnet18.py
+++ src/models/resnet18.py
@@ -1,11 +1,25 @@
+import torch.nn as nn
+from torchvision.models import resnet18, ResNet18_Weights
+
 from .registry import register_model
 
 
 @register_model("resnet18")
-class ResNet18Model:
+class ResNet18Model(nn.Module):
 
     def __init__(self, cfg):
-
+        super().__init__()
         self.cfg = cfg
+        num_classes = cfg["model"]["num_classes"]
+        self.model = resnet18(
+            weights=ResNet18_Weights.DEFAULT
+        )
+        in_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(
+            in_features,
+            num_classes
+        )
+        print("Pretrained ResNet18 Loaded")
 
-        print("ResNet18 Model Initialized")
+    def forward(self, x):
+        return self.model(x)
